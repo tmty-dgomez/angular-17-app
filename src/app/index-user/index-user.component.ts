46bc69f781ced9ed1ae4728992a83f295921cd5f
tmty-dgomez <david.gomez@tecnomty.com>
2024-05-24 14:54:19 -0600

    edits

--- src/app/index-user/index-user.component.ts
+++ src/app/index-user/index-user.component.ts
@@ -1,23 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import { NgFor, NgIf } from '@angular/common';
 import { User } from './user';
 import { UsersService } from './users.service';
+import { io } from "socket.io-client";
 
 @Component({
   selector: 'app-index-users',
   standalone: true,
-  imports: [RouterLink, NgIf, NgFor],
+  imports: [RouterLink, NgIf, NgFor,],
   templateUrl: './index-user.component.html',
   styleUrls: ['./index-user.component.css']
 })
-export class IndexUsersComponent {
+export class IndexUsersComponent implements OnInit {
   collection: User[] | undefined;
   loading: boolean = false;
   title: string = 'Usuarios';
 
   constructor(private service: UsersService, private router: Router) {
     this.index();
+  }
+
+  ngOnInit(): void {
+    const socket = io("ws://127.0.0.1:3333");
+
+    socket.on("connect", () => {
+      console.log("Conectado al servidor");
+    });
+
+    socket.on("update:users", () => {
+      this.index();
+    });
   }
 
   index() {
